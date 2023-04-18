2c297306197675d1ca29613b55acbedb2eebe472
Raisyam <rmfbkw04@gmail.com>
2023-04-18 15:50:09 +0700

    change extension

---
+++ src/state.js
@@ -0,0 +1,14 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const messageBoxSlice = createSlice({
+	name: 'message',
+	initialState: '',
+	reducers: {
+		addMessage(state, action) {
+			//
+		},
+		removeMessage(state, action) {
+			//
+		}
+	}
+})
