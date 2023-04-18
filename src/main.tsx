e8619dda2e17c5de3a5b722fb21d71cd5cd852f0
Raisyam <rmfbkw04@gmail.com>
2023-04-19 04:50:00 +0700

    remove redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.scss';
-import { store } from './state';
-import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
